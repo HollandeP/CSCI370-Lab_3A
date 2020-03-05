89d8d293d33953eaaeef388a04bfdf315da5037a
Hollande <hollandepowell@gmail.com>
2020-03-04 19:23:21 -0500

    errors included

--- app/src/main/java/com/example/csci370lab3a/MainActivity.java
+++ app/src/main/java/com/example/csci370lab3a/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         StudentAdapter adapter = new StudentAdapter(this, students);
         studentList.setAdapter(adapter);
+        //
 
 
     }
